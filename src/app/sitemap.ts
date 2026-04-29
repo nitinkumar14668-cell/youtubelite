73e05d78447f87f3f170840028ffe6e99934c548
Indian <nitinkumar14668@gmail.com>
2026-04-29 13:43:56 +0530

    fix: Update base URL in sitemap and robots
    Updates the `NEXT_PUBLIC_BASE_URL` in `sitemap.ts` and `robots.ts` to reflect the new deployment URL, ensuring proper indexing and site navigation.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://yt-clone-omega.vercel.app'; // Replace with your actual domain
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://youtubelitex.vercel.app';
 
   return [
     {
